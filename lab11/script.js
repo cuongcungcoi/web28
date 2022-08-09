ee3a4a18048bd257d8b707e35d056becc28ca44a
Cuongbe0 <cuongbt9x.pro@gmail.com>
2022-08-09 10:20:58 +0700

    lab11

---
+++ lab11/script.js
@@ -0,0 +1,186 @@
+/**
+ * Tìm số lớn nhất trong 3 số
+ *
+ * @param {number} a Số thứ 1
+ * @param {number} b Số thứ 2
+ * @param {number} c Số thứ 3
+ *
+ * @return {number} Số lớn nhất trong 3 số
+ */
+function maxOfThree(a, b, c) {
+  if (a - b >= 0 && a - c >= 0) {
+    return a;
+  } else if (a - b < 0 && a - c > 0) {
+    return b;
+  } else if (a - b > 0 && a - c < 0) {
+    return c;
+  } else {
+    if (b - c > 0) {
+      return b;
+    }
+    return c;
+  }
+}
+
+/**
+ * Tìm mùa tương ứng với tháng
+ *
+ * - Winter (mùa đông): 12, 1, 2
+ * - Spring (mùa xuân): 3, 4, 5
+ * - Summer (mùa hạ): 6, 7, 8
+ * - Fall (mùa thu): 9, 10, 11
+ *
+ * @param {number} month Tháng trong năm
+ *
+ * @return {number} Mùa tương ứng với tháng (Winter | Spring | Summer | Fall)
+ */
+function findSeason(month) {
+  switch (month) {
+    case 1:
+    case 2:
+    case 12:
+      return "Winter";
+    case 3:
+    case 4:
+    case 5:
+      return "Spring";
+    case 6:
+    case 7:
+    case 8:
+      return "Summer";
+    case 9:
+    case 10:
+    case 11:
+      return "Fall";
+    default:
+      return false;
+  }
+}
+
+/**
+ * Kiểm tra xem một năm có phải năm nhuận hay không
+ *
+ * Theo lịch Gregory, năm nhuận là năm có số năm chia hết cho `4` và không chia hết cho `100`,
+ * tuy nhiên ngoại lệ với các năm chia hết cho `100` vẫn được coi là năm nhuận nếu nó cũng chia hết cho `400`
+ *
+ * - `2020` là năm nhuận vì chia hết cho `4` nhưng không chia hết cho `100`
+ * - `2100` không phải năm nhuận vì chia hết cho `4` nhưng chia hết cho `100`
+ * - `2000` là năm nhuận vì chia hết cho `100` nhưng cũng chia hết cho `400`
+ *
+ * @param {number} year Năm bất kỳ
+ *
+ * @return {boolean} Năm này có phải năm nhuận hay không
+ */
+function isLeafYear(year) {
+  return (
+    (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)
+  );
+}
+/**
+ * Tìm số ngày trong tháng
+ *
+ * - Các tháng `1`, `3`, `5`, `7`, `8`, `10`, `12` có `31` ngày
+ * - Các tháng `4`, `6`, `9`, `11` có `30` ngày
+ * - Tháng `2` có `29` ngày nếu là năm nhuận, nếu không là `28` ngày
+ *
+ * @param {number} month Tháng bất kỳ
+ * @param {number} year Năm bất kỳ
+ *
+ * @return {number} Số ngày trong tháng đó
+ */
+function findDayOfMonth(month, year) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    case 2:
+      if (isLeafYear(year)) {
+        return 29;
+      } else return 28;
+    default:
+      return false;
+  }
+}
+
+/**
+ * Xếp loại sinh viên theo số điểm trung bình
+ *
+ * @param {number} point Điểm trung bình của sinh viên
+ *
+ * @return {'A' | 'B' | 'C' | 'D' | 'F'} Xếp hạng tốt nghiệp của sinh viên
+ */
+function calcGrade(point) {
+  if (point > 0 && point < 4) {
+    return "F";
+  } else if (point >= 4 && point < 5.5) {
+    return "D";
+  } else if (point >= 5.5 && point < 7) {
+    return "C";
+  } else if (point >= 7 && point < 8.5) {
+    return "B";
+  } else if (point >= 8.5 && point < 10) {
+    return "A";
+  } else return false;
+}
+
+/**
+ * Tính tiền cước taxi cho khách theo số kilomet đã di chuyển
+ *
+ * - Phí mở cửa cố định là `10.000đ`
+ * - Giá cho `30km` đầu tiên là `11.000đ/km`
+ * - Từ kilomet thứ `31` giá là `9.500đ/km`
+ * - Tổng tiền cước = Phí mở cửa + tổng tiền theo số kilomet đã di chuyển
+ *
+ * @param {number} km Số kilomet đã di chuyển
+ *
+ * @return {number} Số tiền phải trả
+ */
+function calcTaxiFee(km) {
+  if (km <= 30) {
+    return 10_000 + km * 11_000 + "đ";
+  } else if (km >= 30) {
+    return 10_000 + 30 * 11_000 + (km - 30) * 9_500 + "đ";
+  } else return false;
+}
+console.log(calcTaxiFee(32));
+
+/**
+ * Tìm nghiệm phương trình bậc 2 ax^2 + bx + c = 0
+ *
+ * - Nếu phương trình có vô số nghiệm thì trả về `Infinity`
+ * - Nếu phương trình vô nghiệm thì trả về `null`
+ * - Nếu phương trình có 1 nghiệm thì trả về nghiệm đó
+ * - Nếu phương trình có 2 nghiệm thì trả về một *mảng* chứa 2 nghiệm, cú pháp `[x1, x2]`
+ *
+ * @param {number} a Tham số a
+ * @param {number} b Tham số b
+ * @param {number} c Tham số c
+ *
+ * @return {number | [number, number] | null} Nghiệm phương trình
+ */
+function solveEquation(a, b, c) {
+  let delta = Math.pow(b, 2) - 4 * a * c;
+  if (a != 0) {
+    if (delta < 0) {
+      return null;
+    } else if (delta == 0) {
+      return (x = -b / (2 * a));
+    } else {
+      return [
+        (x1 = (-b + Math.pow(delta, 0.5)) / (2 * a)),
+        (x2 = (-b - Math.pow(delta, 0.5)) / (2 * a)),
+      ];
+    }
+  } else return Infinity;
+}
+console.log(solveEquation(1, -5, 6));
--- lab9/demo.js
+++ lab9/demo.js
@@ -1,19 +1,13 @@
-function info1(a, b) {
-  return (s = a + b);
-}
+alert("Viet Nam Vo Dich");
 
-function info2(a, b) {
-  return (s2 = a - b);
-}
-
-function info3(a, b) {
-  return (s3 = a * b);
-}
-function info4(a, b) {
-  return (s4 = a / b);
-}
-info1(3, 5);
-info2(3, 5);
-info3(3, 5);
-info4(3, 5);
-console.log(s, s2, s3, s4);
+let answer = prompt("Who's there?");
+if (answer === "" || answer === null) {
+  alert("Cancelled");
+} else if (answer === "Admin") {
+  let pass = prompt("Password");
+  if (pass === "" || pass === null) {
+    alert("Cancelled");
+  } else if (pass === "TheMaster") {
+    alert("Wellcome");
+  } else alert("Wrong password");
+} else alert("I dont know you");

26db92d7bd6b310262f3c4e8701ae02f5332a01b
Cuongbe0 <cuongbt9x.pro@gmail.com>
2022-09-13 09:12:38 +0700

    doglist

--- webAdvantage/B5/Dogselector/dog.js
+++ webAdvantage/B5/Dogselector/dog.js
@@ -6,6 +6,7 @@ const IMG = "https://dog.ceo/api/breed/hound/afghan/images";
 let dogArea = document.getElementById("dog");
 let subDogArea = document.querySelector(".list-sub-dog");
 let subDogList = document.querySelector(".dog");
+let imgArea = document.querySelector(".img-area");
 async function getDog() {
   try {
     let data = await axios.get(DOG_URL);
@@ -13,14 +14,29 @@ async function getDog() {
     const listDog = Object.keys(data.data.message);
     renderDog(listDog);
     const subDog = data.data.message;
-    let index = dogArea.value;
-    console.log(index);
-    subDogList.addEventListener("click", () => renderSubDog(subDog[index]));
+    subDogList.addEventListener("click", () => {
+      let index = dogArea.value;
+      renderSubDog(subDog[index]);
+    });
   } catch (e) {
     console.log(e);
   }
 }
+async function getImg() {
+  let index = dogArea.value;
+  let linkImg = await axios.get(
+    `https://dog.ceo/api/breed/${index}/images/random`
+  );
+  let link = linkImg.data.message;
+  console.log(link);
+  createImg(link);
+}
 
+function createImg(link) {
+  let img = document.createElement("img");
+  img.src = link;
+  imgArea.appendChild(img);
+}
 function renderDog(arr) {
   dogArea.innerHTML = arr
     .map((dog) => `<option value="${dog}" class="dog-type">${dog}</option>`)
@@ -31,7 +47,11 @@ function renderSubDog(arr) {
     subDogArea.innerHTML = `<li >No Sub Breed </li>`;
   } else {
     subDogArea.innerHTML = arr
-      .map((dog) => `<li class="sub-dog-${dog}">${dog}</li>`)
+      .map(
+        (dog) =>
+          `<li class="sub-dog-${dog}"><a onclick ="getImg()">${dog}</a></li>`
+      )
       .join("");
   }
 }
+getDog();
